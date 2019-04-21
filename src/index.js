fb9a17ad6b3cd600f71d824179c2f4ee94999112
Safdar Jamal <cleverprogrammer.me@gmail.com>
2019-04-21 18:29:35 +0500

    Added semantic-ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
