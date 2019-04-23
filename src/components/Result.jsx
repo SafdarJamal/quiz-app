1ab1f3f1cd533a3532b751fb670cab397604e663
Safdar Jamal <cleverprogrammer.me@gmail.com>
2019-04-23 22:24:42 +0500

    Succesfully rendering result component

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Header, Icon, Segment, Container } from 'semantic-ui-react';
+import { Container, Segment, Label, Header, Button } from 'semantic-ui-react';
 
 class Result extends Component {
   constructor(props) {
@@ -11,15 +11,34 @@ class Result extends Component {
     return (
       <div>
         <Container>
-          <Segment placeholder>
-            <Header icon>
-              <Icon name="search" />
-              We don't have any documents matching your query.
+          <Segment raised>
+            <Label as="a" color="red" ribbon>
+              Result
+            </Label>
+            <Header as="h1" textAlign="center" block>
+              Congratulations, YOU PASSED!
+              {/* Sorry, YOU FAILED! */}
             </Header>
-            <Segment.Inline>
-              <Button primary>Clear Query</Button>
-              <Button>Add Document</Button>
-            </Segment.Inline>
+            <Header as="h3" textAlign="center" block>
+              Passing Score: 60
+            </Header>
+            <Header as="h3" textAlign="center" block>
+              Total Questions: 10
+            </Header>
+            <Header as="h3" textAlign="center" block>
+              Correct Answers: 7
+            </Header>
+            <Header as="h3" textAlign="center" block>
+              Your Score: 70
+            </Header>
+            <div style={{ marginTop: '25px', marginBottom: '8px' }}>
+              <Button primary size="large">
+                Retake Quiz
+              </Button>
+              <Button color="teal" size="large">
+                Back to Home
+              </Button>
+            </div>
           </Segment>
         </Container>
       </div>
