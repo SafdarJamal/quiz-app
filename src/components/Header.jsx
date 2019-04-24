aed4f5bb2de72231131606cf7ce88c7c114a16b8
Safdar Jamal <cleverprogrammer.me@gmail.com>
2019-04-24 17:32:35 +0500

    Added favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Header, Segment } from 'semantic-ui-react';
-import logo from '../assets/logo.svg';
+import logo from '../assets/logo.png';
 
 export default () => (
   <Segment clearing inverted raised>
     <Header as="h1" floated="left" color="blue">
-      <img src={logo} alt="Quiz App" />
+      <img src={logo} alt="Quiz App" width="5px" height="5px" />
       Quiz App
     </Header>
   </Segment>
