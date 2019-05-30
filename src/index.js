3d5b0ad6b0ebba984b14db3af0b6196a84a698a9
Safdar Jamal <safdar.dev@gmail.com>
2019-05-30 21:25:21 +0500

    Correct links in App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
-import App from './App.jsx';
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
