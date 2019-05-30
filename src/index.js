a786ce91b3af61fa079137e16454f0c8041fa4f0
Safdar Jamal <safdar.dev@gmail.com>
2019-05-30 21:49:22 +0500

    Remove hot.module

--- src/index.js
+++ src/index.js
@@ -4,7 +4,3 @@ import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-if (module.hot) {
-  module.hot.accept();
-}
