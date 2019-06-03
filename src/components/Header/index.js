9023ab4be776a409986bfb89c6daff4310595b3e
Safdar Jamal <safdar.dev@gmail.com>
2019-06-04 00:44:27 +0500

    Convert Header to class component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,51 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Menu, Button } from 'semantic-ui-react';
 
-export default () => {
-  return (
-    <Menu stackable inverted size="massive">
-      <Menu.Item actives>
-        <h1
-          style={{
-            color: '#2185D0',
-            cursor: 'pointer'
-          }}
-        >
-          The QuizApp
-        </h1>
-      </Menu.Item>
+class Header extends Component {
+  constructor(props) {
+    super(props);
 
-      <Menu.Menu position="right">
-        <Menu.Item>
-          <Button
-            color="teal"
-            content="Install App"
-            size="big"
-            icon="app store"
-            labelPosition="left"
-            onClick={installApp}
-          />
+    this.promptEvent = null;
+
+    window.addEventListener('beforeinstallprompt', e => {
+      e.preventDefault();
+      this.promptEvent = e;
+    });
+  }
+
+  installApp() {
+    console.log('Init Func ===', this.promptEvent);
+
+    this.promptEvent.prompt();
+    this.promptEvent.userChoice.then(result => {
+      if (result.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+        console.log('==>', this.promptEvent);
+      } else {
+        console.log('User dismissed the A2HS prompt');
+        console.log('==>', this.promptEvent);
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Menu stackable inverted size="massive">
+        <Menu.Item actives>
+          <h1
+            style={{
+              color: '#2185D0',
+              cursor: 'pointer'
+            }}
+          >
+            The QuizApp
+          </h1>
         </Menu.Item>
-      </Menu.Menu>
-    </Menu>
-  );
-};
 
-let promptEvent;
-
-window.addEventListener('beforeinstallprompt', e => {
-  e.preventDefault();
-  promptEvent = e;
-});
-
-function installApp() {
-  promptEvent.prompt();
-  promptEvent.userChoice.then(result => {
-    if (result.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-      console.log('==>', promptEvent.outcome);
-    }
-  });
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Button
+              color="teal"
+              content="Install App"
+              size="big"
+              icon="app store"
+              labelPosition="left"
+              onClick={this.installApp}
+            />
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    );
+  }
 }
+
+// let promptEvent;
+
+// window.addEventListener('beforeinstallprompt', e => {
+//   e.preventDefault();
+//   promptEvent = e;
+// });
+
+export default Header;
