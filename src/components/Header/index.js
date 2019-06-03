a1a3655476e0df81b974aa3de10b591af32dfd51
Safdar Jamal <safdar.dev@gmail.com>
2019-06-03 21:05:26 +0500

    Rebuild header acording to pwa

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Header, Segment } from 'semantic-ui-react';
-import logo from '../../assets/images/logo.png';
+import { Menu, Button } from 'semantic-ui-react';
 
-export default () => (
-  <Segment clearing inverted raised>
-    <Header as="h2" floated="left" color="blue">
-      <img src={logo} alt="Quiz App" />
-      Quiz App
-    </Header>
-  </Segment>
-);
+export default () => {
+  return (
+    <Menu stackable inverted size="massive">
+      <Menu.Item actives>
+        <h1
+          style={{
+            color: '#2185D0',
+            cursor: 'pointer'
+          }}
+        >
+          The QuizApp
+        </h1>
+      </Menu.Item>
+
+      <Menu.Menu position="right">
+        <Menu.Item>
+          <Button
+            color="teal"
+            content="Install App"
+            size="big"
+            icon="app store"
+            labelPosition="left"
+          />
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  );
+};
