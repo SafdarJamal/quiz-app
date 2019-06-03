df3b103906c35ec766f84646409b576bc3260038
Safdar Jamal <safdar.dev@gmail.com>
2019-06-04 00:27:34 +0500

    Write function for install event

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -23,9 +23,29 @@ export default () => {
             size="big"
             icon="app store"
             labelPosition="left"
+            onClick={installApp}
           />
         </Menu.Item>
       </Menu.Menu>
     </Menu>
   );
 };
+
+let promptEvent;
+
+window.addEventListener('beforeinstallprompt', e => {
+  e.preventDefault();
+  promptEvent = e;
+});
+
+function installApp() {
+  promptEvent.prompt();
+  promptEvent.userChoice.then(result => {
+    if (result.outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt');
+    } else {
+      console.log('User dismissed the A2HS prompt');
+      console.log('==>', promptEvent.outcome);
+    }
+  });
+}
