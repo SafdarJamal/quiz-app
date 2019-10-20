e20cf202ba0c77d0a6ec14c97e454d71a5a7a238
Safdar Jamal <safdar.dev@gmail.com>
2019-10-20 22:21:52 +0500

    ShareButton complete

--- src/components/ShareButton/index.js
+++ src/components/ShareButton/index.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Button } from 'semantic-ui-react';
 
 const ShareButton = () => {
   const doShare = () => {
-    if (navigator.share) {
-      navigator
-        .share({
-          text: 'Check out the QuizApp — it rocks!',
-          url: 'https://safdarjamal.github.io/quiz-app'
-        })
-        .then(() => {
-          console.log('Successfully shared');
-        })
-        .catch(error => console.log(error));
-    } else {
-      console.log('Sharing Not available');
-    }
+    navigator
+      .share({
+        text: 'Check out the QuizApp — it rocks!',
+        url: 'https://safdarjamal.github.io/quiz-app'
+      })
+      .then(() => {
+        // console.log('Successfully shared');
+      })
+      .catch(error => console.log(error));
   };
 
   return (
-    <Button
-      title="Share"
-      floated="right"
-      size="big"
-      circular
-      icon="share alternate"
-      onClick={doShare}
-    />
+    <Fragment>
+      {navigator.share ? (
+        <Button
+          title="Share"
+          floated="right"
+          size="big"
+          circular
+          icon="share alternate"
+          onClick={doShare}
+        />
+      ) : null}
+    </Fragment>
   );
 };
 
