c718b49cc1375a249e11429b5a363a8379619c84
Safdar Jamal <safdar.dev@gmail.com>
2020-11-15 01:20:32 +0500

    Refactor Loader component

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Container, Message, Icon } from 'semantic-ui-react';
 
-const Loader = ({ text }) => (
-  <Container>
-    <Message icon size="big">
-      <Icon name="circle notched" loading />
-      <Message.Content>
-        <Message.Header>Just one second</Message.Header>
-        {text || 'We are fetching that content for you.'}
-      </Message.Content>
-    </Message>
-  </Container>
-);
+const Loader = ({ text }) => {
+  return (
+    <Container>
+      <Message icon size="big">
+        <Icon name="circle notched" loading />
+        <Message.Content>
+          <Message.Header>Just one second</Message.Header>
+          {text || 'We are fetching that content for you.'}
+        </Message.Content>
+      </Message>
+    </Container>
+  );
+};
 
 export default Loader;
