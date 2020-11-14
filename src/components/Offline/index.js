decfb08b55b81d9c4bbf0e3f6c415bbe09108578
Safdar Jamal <safdar.dev@gmail.com>
2020-11-14 21:38:03 +0500

    Refactor Offline component

--- src/components/Offline/index.js
+++ src/components/Offline/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Container, Segment, Header, Icon } from 'semantic-ui-react';
 
-const Offline = () => (
-  <Container>
-    <Segment placeholder>
-      <Header icon>
-        <Icon name="unlink" />
-        <br />
-        It looks like you're offline, Please check your internet connection.
-      </Header>
-    </Segment>
-  </Container>
-);
+const Offline = () => {
+  return (
+    <Container>
+      <Segment placeholder>
+        <Header icon>
+          <Icon name="unlink" />
+          <br />
+          It looks like you're offline, please check your internet connection.
+        </Header>
+      </Segment>
+    </Container>
+  );
+};
 
 export default Offline;
