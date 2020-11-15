54ff0db6e28b1c249f54b7c3da6fbd2611f502a1
Safdar Jamal <safdar.dev@gmail.com>
2020-11-15 19:52:47 +0500

    Refactor Main component

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
   Container,
   Segment,
@@ -7,8 +7,8 @@ import {
   Divider,
   Button
 } from 'semantic-ui-react';
-import codeImg from '../../images/code.png';
 
+import codeImg from '../../images/code.png';
 import {
   CATEGORIES,
   NUM_OF_QUESTIONS,
@@ -17,141 +17,115 @@ import {
   COUNTDOWN_TIME
 } from '../../constants';
 
-class Main extends Component {
-  constructor(props) {
-    super(props);
+const Main = ({ startQuiz }) => {
+  const [category, setCategory] = useState(null);
+  const [numOfQ, setNumOfQ] = useState(null);
+  const [difficulty, setDifficulty] = useState(null);
+  const [type, setType] = useState(null);
+  const [time, setTime] = useState(null);
 
-    this.state = {
-      category: null,
-      numOfQ: null,
-      difficulty: null,
-      type: null,
-      time: null
+  let allFieldsSelected = false;
+  let selectedValues = null;
+
+  if (category && numOfQ && difficulty && type && time) {
+    allFieldsSelected = true;
+
+    selectedValues = {
+      category,
+      numOfQ,
+      difficulty,
+      type,
+      time
     };
-
-    this.setValue = this.setValue.bind(this);
   }
 
-  setValue(name, value) {
-    this.setState({ [name]: value });
-  }
-
-  render() {
-    const { category, numOfQ, difficulty, type, time } = this.state;
-
-    let allFieldsSelected = false;
-    let selectedValues = null;
-
-    if (category && numOfQ && difficulty && type && time) {
-      allFieldsSelected = true;
-
-      selectedValues = {
-        category,
-        numOfQ,
-        difficulty,
-        type,
-        time
-      };
-    }
-
-    return (
-      <Container>
-        <Segment>
-          <Item.Group divided>
-            <Item>
-              <Item.Image src={codeImg} />
-              <Item.Content>
-                <Item.Header>
-                  <h1>Open Trivia Questions</h1>
-                </Item.Header>
+  return (
+    <Container>
+      <Segment>
+        <Item.Group divided>
+          <Item>
+            <Item.Image src={codeImg} />
+            <Item.Content>
+              <Item.Header>
+                <h1>Open Trivia Questions</h1>
+              </Item.Header>
+              <br />
+              <Divider />
+              <Item.Meta>
+                <Dropdown
+                  fluid
+                  selection
+                  name="category"
+                  placeholder="Select Quiz Category"
+                  options={CATEGORIES}
+                  onChange={(e, { value }) => setCategory(value)}
+                />
                 <br />
-                <Divider />
-                <Item.Meta>
-                  <Dropdown
-                    fluid
-                    selection
-                    name="category"
-                    placeholder="Select Quiz Category"
-                    options={CATEGORIES}
-                    onChange={(e, { name, value }) =>
-                      this.setValue(name, value)
-                    }
+                <Dropdown
+                  fluid
+                  selection
+                  name="numOfQ"
+                  placeholder="Select No. of Questions"
+                  options={NUM_OF_QUESTIONS}
+                  onChange={(e, { value }) => setNumOfQ(value)}
+                />
+                <br />
+                <Dropdown
+                  fluid
+                  selection
+                  name="difficulty"
+                  placeholder="Select Difficulty Level"
+                  options={DIFFICULTY}
+                  onChange={(e, { value }) => setDifficulty(value)}
+                />
+                <br />
+                <Dropdown
+                  fluid
+                  selection
+                  name="type"
+                  placeholder="Select Questions Type"
+                  options={QUESTIONS_TYPE}
+                  onChange={(e, { value }) => setType(value)}
+                />
+                <br />
+                <Dropdown
+                  fluid
+                  selection
+                  name="time"
+                  placeholder="Select Countdown Time (In Minutes)"
+                  options={COUNTDOWN_TIME}
+                  onChange={(e, { value }) => setTime(value)}
+                />
+              </Item.Meta>
+              <Divider />
+              <Item.Extra>
+                {allFieldsSelected ? (
+                  <Button
+                    primary
+                    content="Start Quiz"
+                    onClick={() => startQuiz(selectedValues)}
+                    size="big"
+                    icon="play"
+                    labelPosition="left"
                   />
-                  <br />
-                  <Dropdown
-                    fluid
-                    selection
-                    name="numOfQ"
-                    placeholder="Select No. of Questions"
-                    options={NUM_OF_QUESTIONS}
-                    onChange={(e, { name, value }) =>
-                      this.setValue(name, value)
-                    }
+                ) : (
+                  <Button
+                    disabled
+                    primary
+                    content="Start Quiz"
+                    size="big"
+                    icon="play"
+                    labelPosition="left"
                   />
-                  <br />
-                  <Dropdown
-                    fluid
-                    selection
-                    name="difficulty"
-                    placeholder="Select Difficulty Level"
-                    options={DIFFICULTY}
-                    onChange={(e, { name, value }) =>
-                      this.setValue(name, value)
-                    }
-                  />
-                  <br />
-                  <Dropdown
-                    fluid
-                    selection
-                    name="type"
-                    placeholder="Select Questions Type"
-                    options={QUESTIONS_TYPE}
-                    onChange={(e, { name, value }) =>
-                      this.setValue(name, value)
-                    }
-                  />
-                  <br />
-                  <Dropdown
-                    fluid
-                    selection
-                    name="time"
-                    placeholder="Select Countdown Time (In Minutes)"
-                    options={COUNTDOWN_TIME}
-                    onChange={(e, { name, value }) =>
-                      this.setValue(name, value)
-                    }
-                  />
-                </Item.Meta>
-                <Divider />
-                <Item.Extra>
-                  {allFieldsSelected ? (
-                    <Button
-                      primary
-                      content="Start Quiz"
-                      onClick={() => this.props.startQuiz(selectedValues)}
-                      size="big"
-                      icon="play"
-                      labelPosition="left"
-                    />
-                  ) : (
-                    <Button
-                      disabled
-                      primary
-                      content="Start Quiz"
-                      size="big"
-                      icon="play"
-                      labelPosition="left"
-                    />
-                  )}
-                </Item.Extra>
-              </Item.Content>
-            </Item>
-          </Item.Group>
-        </Segment>
-        <br />
-      </Container>
-    );
-  }
-}
+                )}
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+      <br />
+    </Container>
+  );
+};
 
 export default Main;
