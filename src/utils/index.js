766a68d9d6b498e2ea2621a3e93fb2e3c6f40ef4
Safdar Jamal <safdar.dev@gmail.com>
2020-12-09 19:30:52 +0500

    Create calculateScore utility

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
-export { default as timeConverter } from './timeConverter';
-export { default as getRandomNumber } from './getRandomNumber';
 export { default as calculateGrade } from './calculateGrade';
+export { default as calculateScore } from './calculateScore';
+export { default as getRandomNumber } from './getRandomNumber';
+export { default as timeConverter } from './timeConverter';
