f5bb8e05146f4c97174eaf07f8cb8e3a17e90d66
Safdar Jamal <safdar.dev@gmail.com>
2020-12-10 20:52:51 +0500

    Update constants index

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export { default as CATEGORIES } from './categories';
-export { default as NUM_OF_QUESTIONS } from './numOfQuestions';
-export { default as DIFFICULTY } from './difficulty';
-export { default as QUESTIONS_TYPE } from './questionsType';
 export { default as COUNTDOWN_TIME } from './countdownTime';
+export { default as DIFFICULTY } from './difficulty';
+export { default as NUM_OF_QUESTIONS } from './numOfQuestions';
+export { default as QUESTIONS_TYPE } from './questionsType';
