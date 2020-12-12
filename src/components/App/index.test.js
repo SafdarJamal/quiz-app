0fbde220159b403f510f322ee0ff4b767e209e12
Safdar Jamal <safdar.dev@gmail.com>
2020-12-12 21:17:05 +0500

    Update App test

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '.';
 
-test('renders without crashing', () => {
-  const { unmount } = render(<App />);
-  unmount();
+describe('<App />', () => {
+  test('renders without crashing', () => {
+    const { unmount } = render(<App />);
+    unmount();
+  });
 });
