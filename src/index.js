f21dfd720b6d0154a0d9a3e7893fcdea39a3261a
Safdar Jamal <safdar.dev@gmail.com>
2021-01-01 19:21:26 +0500

    Create styles index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
+import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
