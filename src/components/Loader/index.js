bd89d6c90b59f81e50f73b15f5a2f756c31a6867
Safdar Jamal <safdar.dev@gmail.com>
2021-01-02 21:55:31 +0500

    Update Loader component

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Container, Message, Icon } from 'semantic-ui-react';
 
-const Loader = ({ text }) => {
+const Loader = () => {
   return (
     <Container>
       <Message icon size="big">
         <Icon name="circle notched" loading />
         <Message.Content>
           <Message.Header>Just one second</Message.Header>
-          {text || 'We are fetching that content for you.'}
+          We are fetching that content for you.
         </Message.Content>
       </Message>
     </Container>
