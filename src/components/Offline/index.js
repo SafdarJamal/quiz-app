8b275c6317d803689059d960dec952aee4e8eb1f
Safdar Jamal <safdar.dev@gmail.com>
2021-01-05 23:58:38 +0500

    Enhance UI/UX for offline usage

--- src/components/Offline/index.js
+++ src/components/Offline/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { Container, Segment, Header, Icon } from 'semantic-ui-react';
 
 const Offline = () => {
+  window.addEventListener('online', () => window.location.reload());
+
   return (
     <Container>
       <Segment placeholder>
         <Header icon>
           <Icon name="unlink" />
-          <br />
-          It looks like you're offline, please check your internet connection.
+          <h1>Offline</h1>
+          <p>
+            There is no Internet connection. We'll try to reload automatically
+            once you're back online!{' '}
+            <span role="img" aria-label="signal">
+              📶
+            </span>
+          </p>
         </Header>
       </Segment>
     </Container>
