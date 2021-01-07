59a04562e1c6b1d751732b06c3528ab15fba4592
Safdar Jamal <safdar.dev@gmail.com>
2021-01-07 21:56:30 +0500

    Create UI modal for web sharing

--- src/components/ShareButton/index.js
+++ src/components/ShareButton/index.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react';
-import { Button, Icon } from 'semantic-ui-react';
+import { Button, Icon, Modal } from 'semantic-ui-react';
 
 const ShareButton = () => {
-  const handleShare = () => {
+  const handleClick = () => {
     navigator
       .share({
         title: document.title,
@@ -22,41 +22,56 @@ const ShareButton = () => {
           size="big"
           circular
           icon="share alternate"
-          onClick={handleShare}
+          onClick={handleClick}
         />
       ) : (
-        <span>
-          <a
-            href="https://www.facebook.com/sharer.php?u=https%3A//safdarjamal.github.io/quiz-app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Button color="facebook" size="big" floated="right">
-              <Icon name="facebook" />
-              Facebook
-            </Button>
-          </a>
-          <a
-            href="https://twitter.com/intent/tweet?url=https%3A//safdarjamal.github.io/quiz-app/&text=Check%20out%20this%20quiz%20app%20—%20it%20rocks!"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Button color="twitter" size="big" floated="right">
-              <Icon name="twitter" />
-              Twitter
-            </Button>
-          </a>
-          <a
-            href="https://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fsafdarjamal.github.io%2Fquiz-app%2F"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Button color="linkedin" size="big" floated="right">
-              <Icon name="linkedin" />
-              LinkedIn
-            </Button>
-          </a>
-        </span>
+        <Modal
+          closeIcon
+          size="tiny"
+          trigger={
+            <Button
+              title="Share"
+              floated="right"
+              size="big"
+              circular
+              icon="share alternate"
+            />
+          }
+        >
+          <Modal.Header className="ui center aligned">Share on</Modal.Header>
+          <Modal.Content className="ui center aligned container">
+            <a
+              href="https://www.facebook.com/sharer.php?u=https%3A//safdarjamal.github.io/quiz-app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button color="facebook" size="big">
+                <Icon name="facebook" />
+                Facebook
+              </Button>
+            </a>
+            <a
+              href="https://twitter.com/intent/tweet?url=https%3A//safdarjamal.github.io/quiz-app/&text=Check%20out%20this%20quiz%20app%20—%20it%20rocks!&via=_safdarjamal"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button color="twitter" size="big">
+                <Icon name="twitter" />
+                Twitter
+              </Button>
+            </a>
+            <a
+              href="https://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fsafdarjamal.github.io%2Fquiz-app%2F"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button color="linkedin" size="big">
+                <Icon name="linkedin" />
+                LinkedIn
+              </Button>
+            </a>
+          </Modal.Content>
+        </Modal>
       )}
     </Fragment>
   );
