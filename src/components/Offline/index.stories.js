90034be7124833fcba8837e28d460b17ef570ecf
Safdar Jamal <safdar.dev@gmail.com>
2022-12-26 20:45:26 +0500

    Create stories for Offline component

---
+++ src/components/Offline/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import OfflineComponent from './index';
+
+export default {
+  title: 'Components/Offline',
+  component: OfflineComponent,
+};
+
+export const Offline = <OfflineComponent />;
