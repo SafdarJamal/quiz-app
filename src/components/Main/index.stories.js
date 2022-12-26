d5559ed698778c95d46b0e98fc5805c5df85efdd
Safdar Jamal <safdar.dev@gmail.com>
2022-12-26 21:08:02 +0500

    Create stories for Main component

---
+++ src/components/Main/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MainComponent from './index';
+
+export default {
+  title: 'Components/Main',
+  component: MainComponent,
+};
+
+export const Main = () => <MainComponent startQuiz={() => {}} />;
