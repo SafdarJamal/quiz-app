00229f6e8a69ec723fda29e0fdbff77553da2d80
Safdar Jamal <safdar.dev@gmail.com>
2022-12-26 21:26:32 +0500

    Create stories for Result component

---
+++ src/components/Result/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ResultComponent from './index';
+
+export default {
+  title: 'Components/Result',
+  component: ResultComponent,
+};
+
+export const Result = () => <ResultComponent />;
