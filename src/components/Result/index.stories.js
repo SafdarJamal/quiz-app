13ced2b98f994324abaafecb6cd327d77e8e68ed
Safdar Jamal <safdar.dev@gmail.com>
2022-12-26 21:27:40 +0500

    Add controls on Result stories

--- src/components/Result/index.stories.js
+++ src/components/Result/index.stories.js
@@ -6,4 +6,21 @@ export default {
   component: ResultComponent,
 };
 
-export const Result = () => <ResultComponent />;
+const Template = args => <ResultComponent {...args} />;
+export const Result = Template.bind({});
+
+Result.args = {
+  totalQuestions: 5,
+  correctAnswers: 4,
+  timeTaken: 100 * 1000,
+  questionsAndAnswers: [
+    {
+      question: 'he.decode(data[questionIndex].question)',
+      user_answer: 'userSlectedAns',
+      correct_answer: 'he.decode(data[questionIndex].correct_answer)',
+      point: 1,
+    },
+  ],
+  replayQuiz: () => {},
+  resetQuiz: () => {},
+};
