3d64f2c3d0a4760dbc02f89b9bae0abba8548c33
Safdar Jamal <safdar.dev@gmail.com>
2023-06-15 23:50:50 +0500

    Refactor Offline story

--- src/components/Offline/index.stories.js
+++ src/components/Offline/index.stories.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import OfflineComponent from './index';
 
 export default {
@@ -6,4 +5,4 @@ export default {
   component: OfflineComponent,
 };
 
-export const Offline = <OfflineComponent />;
+export const Offline = {};
