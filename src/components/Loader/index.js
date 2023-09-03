10fd1fd403a93adbf49e59313144ccf809c84617
Safdar Jamal <safdar.dev@gmail.com>
2023-09-03 19:19:27 +0500

    Refactor Loader component
    - Refactor Loader component to use personalized loading messages

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Container, Message, Icon } from 'semantic-ui-react';
 
-const Loader = () => {
-  return (
-    <Container>
-      <Message icon size="big">
-        <Icon name="circle notched" loading />
-        <Message.Content>
-          <Message.Header>Just one second</Message.Header>
-          We are fetching that content for you.
-        </Message.Content>
-      </Message>
-    </Container>
-  );
-};
+const Loader = ({ title, message }) => (
+  <Container>
+    <Message icon size="big">
+      <Icon name="circle notched" loading />
+      <Message.Content>
+        <Message.Header>{title ? title : 'Just one second'}</Message.Header>
+        {message ? message : 'We are fetching that content for you.'}
+      </Message.Content>
+    </Message>
+  </Container>
+);
 
 export default Loader;
